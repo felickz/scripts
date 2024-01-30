ab2132e25c91ac98cbd4f430054ecbc14684adde
felickz <1760475+felickz@users.noreply.github.com>
2024-01-30 17:38:34 -0500

    Markdown output

--- pwsh/Count-GHSAWithVEA.ps1
+++ pwsh/Count-GHSAWithVEA.ps1
@@ -82,9 +82,66 @@ $results = foreach ($ecosystem in $ecosystems) {
 
 Write-Host "Advisory DB VEA Inventory  $($(Get-Date -AsUTC).ToString('u'))"
 # Output the results as a table
-$results | Sort-Object -Property Count -Descending | Format-Table -AutoSize
+$results | Sort-Object -Property Count -Descending | ConvertTo-Markdown
 
 
+#https://www.powershellgallery.com/packages/PSMarkdown/1.1/Content/ConvertTo-Markdown.ps1
+Function ConvertTo-Markdown {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param (
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [PSObject[]]$InputObject
+    )
 
+    Begin {
+        $items = @()
+        $columns = @{}
+    }
+
+    Process {
+        ForEach($item in $InputObject) {
+            $items += $item
+
+            $item.PSObject.Properties | %{
+                if($_.Value -ne $null){
+                    if(-not $columns.ContainsKey($_.Name) -or $columns[$_.Name] -lt $_.Value.ToString().Length) {
+                        $columns[$_.Name] = $_.Value.ToString().Length
+                    }
+                }
+            }
+        }
+    }
+
+    End {
+        ForEach($key in $($columns.Keys)) {
+            $columns[$key] = [Math]::Max($columns[$key], $key.Length)
+        }
+
+        $header = @()
+        ForEach($key in $columns.Keys) {
+            $header += ('{0,-' + $columns[$key] + '}') -f $key
+        }
+        $header -join ' | '
+
+        $separator = @()
+        ForEach($key in $columns.Keys) {
+            $separator += '-' * $columns[$key]
+        }
+        $separator -join ' | '
+
+        ForEach($item in $items) {
+            $values = @()
+            ForEach($key in $columns.Keys) {
+                $values += ('{0,-' + $columns[$key] + '}') -f $item.($key)
+            }
+            $values -join ' | '
+        }
+    }
+}
 
 
