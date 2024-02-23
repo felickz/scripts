d0746c82409e3813ad960d6cee09d6fc7886dc37
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-02-23 14:08:27 -0500

    Add name of repo to output and enhacne docs

--- pwsh/Get-GitHubReposWithFailingCodeQL.ps1
+++ pwsh/Get-GitHubReposWithFailingCodeQL.ps1
@@ -1,18 +1,35 @@
-# find any CodeQL named runs and get their latest status.. i took some liberty in this sample but if you wanted to search for all repos in an org who have codeql running in an action and see if the scans were building successful
+# find any CodeQL named runs and get their latest status
+# - use the gh cli (easy auth and query apis)
+# - search for all repos in an org
+# - workflows named "CodeQl" running in an action - EXCLUDE PRs
+# - Report if the scans were building successful or not (conclusion)
 
 #results:
-# success - https://github.com/vulna-felickz/WebGoat.NET/actions/runs/3497120617
-#success - https://github.com/vulna-felickz/WebGoat.NET-CORE/actions/runs/6383441854
-#success - https://github.com/vulna-felickz/log4shell-vulnerable-app/actions/runs/5289516341
-#success - https://github.com/vulna-felickz/reactvulna/actions/runs/5780665366
-#success - https://github.com/vulna-felickz/FullDotNetWebApp/actions/runs/5173725533
-#success - https://github.com/vulna-felickz/DotNetCoreWebApp/actions/runs/4483110033
-#success - https://github.com/vulna-felickz/code-scanning-javascript-demo/actions/runs/2633836803
-#success - https://github.com/vulna-felickz/BenchmarkJava/actions/runs/2685850607
-#failure - https://github.com/vulna-felickz/babel/actions/runs/6376049500
-#success - https://github.com/vulna-felickz/Damn-Vulnerable-GraphQL-Application/actions/runs/2819556620
-#failure - https://github.com/vulna-felickz/DotNetCoreWebAPI/actions/runs/6387549363
-#success - https://github.com/vulna-felickz/WebGoat/actions/runs/5372117418
-#success - https://github.com/vulna-felickz/railsgoat/actions/runs/4793129543
-#success - https://github.com/vulna-felickz/VulnerableApp/actions/runs/4401479230
-$org = "vulna-felickz"; gh api /orgs/$org/repos |  jq -r '.[] | .name' | %{ $name = $_; gh api /repos/$org/$_/actions/workflows | jq '.workflows[] | select(.name=="CodeQL") | .id' | % { gh api /repos/$org/$name/actions/workflows/$_/runs?exclude_pull_requests=true } | jq -r '.workflow_runs[0] | "\(.conclusion) - \(.html_url)"' }
+# success - https://github.com/vulna-felickz/WebGoat.NET/actions/runs/8019411601 (WebGoat.NET)
+# success - https://github.com/vulna-felickz/WebGoat.NET-CORE/actions/runs/7963273004 (WebGoat.NET-CORE)
+# success - https://github.com/vulna-felickz/log4shell-vulnerable-app/actions/runs/5289516341 (log4shell-vulnerable-app)
+# null - null (my-spring-log4j-vuln-sample)
+# success - https://github.com/vulna-felickz/reactvulna/actions/runs/5780665366 (reactvulna)
+# success - https://github.com/vulna-felickz/FullDotNetWebApp/actions/runs/7780125962 (FullDotNetWebApp)
+# success - https://github.com/vulna-felickz/DotNetCoreWebApp/actions/runs/7716451772 (DotNetCoreWebApp)
+# success - https://github.com/vulna-felickz/puma-prey/actions/runs/7946337933 (puma-prey)
+# null - null (code-scanning-javascript-demo)
+# null - null (BenchmarkJava)
+# success - https://github.com/vulna-felickz/babel/actions/runs/7954662269 (babel)
+# null - null (Damn-Vulnerable-GraphQL-Application)
+# success - https://github.com/vulna-felickz/DotNetCoreWebAPI/actions/runs/7967201683 (DotNetCoreWebAPI)
+# success - https://github.com/vulna-felickz/WebGoat/actions/runs/7952160077 (WebGoat)
+# success - https://github.com/vulna-felickz/railsgoat/actions/runs/7967722774 (railsgoat)
+# success - https://github.com/vulna-felickz/VulnerableApp/actions/runs/7954709690 (VulnerableApp)
+# success - https://github.com/vulna-felickz/demo-java/actions/runs/4263592915 (demo-java)
+
+$org = "vulna-felickz"; `
+gh api /orgs/$org/repos `
+| jq -r '.[] | .name' `
+| %{ `
+    $name = $_; gh api /repos/$org/$_/actions/workflows `
+    | jq '.workflows[] | select(.name=="CodeQL") | .id' `
+    | % { gh api /repos/$org/$name/actions/workflows/$_/runs?exclude_pull_requests=true } `
+    | jq -r '.workflow_runs[0] | "\(.conclusion) - \(.html_url)"'`
+    | %{ "$_ ($name)" } `
+}
