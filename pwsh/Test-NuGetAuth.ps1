e19f472f4a25b266be330fac85e21dddce6f6d82
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-08-16 09:09:39 -0400

    Fix issue where name has spaces

--- pwsh/Test-NuGetAuth.ps1
+++ pwsh/Test-NuGetAuth.ps1
@@ -74,9 +74,10 @@ foreach ($source in $sources) {
     #    $packages = nuget list -Source $($source.Name)
 
     try {
+        $startTime = [System.Diagnostics.Stopwatch]::StartNew()
 
         #errors easier to catch in stderror
-        $process = Start-Process nuget -ArgumentList "search", $searchFor, "-Source", $($source.Name), "-NonInteractive" -NoNewWindow -RedirectStandardOutput "output.txt" -RedirectStandardError "error.txt" -PassThru
+        $process = Start-Process nuget -ArgumentList "search", $searchFor, "-Source", "$($source.Name)", "-NonInteractive" -NoNewWindow -RedirectStandardOutput "output.txt" -RedirectStandardError "error.txt" -PassThru
         $process.WaitForExit()
 
         $elapsedTime = $startTime.ElapsedMilliseconds
